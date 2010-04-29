b9ad7378b7862a1e89f6b26d6c7e366f2d37160f
sousk.net <sou.phphp@gmail.com>
2010-04-30 08:24:12 +0900

    update

---
+++ README.markdown
@@ -0,0 +1,14 @@
+flow
+========
+
+*flow* is a typical weblog application running on rails & mongodb.
+
+
+setup Authors
+--------
+
+setup authors from your console.
+  r c
+then
+  Author.create! :name => 'sou', :password => 'yours'
+
--- config/initializers/cookie_verification_secret.rb
+++ config/initializers/cookie_verification_secret.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.cookie_secret = 'ab7f4ba900316faccbfa59a908eb6fdb9345ea3f5db322b0265e601a32d286a6a27a0adb2f01c6b597e38d5d615ba4f917394b55f007ab82264bfce1858650ba'
+Rails.application.config.secret_token = 'ab7f4ba900316faccbfa59a908eb6fdb9345ea3f5db322b0265e601a32d286a6a27a0adb2f01c6b597e38d5d615ba4f917394b55f007ab82264bfce1858650ba'
