cd9a81ec9c0063e2a7dedcb194c6a33d33fd005a
sousk.net <sou.phphp@gmail.com>
2010-05-19 23:38:55 +0900

    show entries on admin/entries

--- app/controllers/admin/entries_controller.rb
+++ app/controllers/admin/entries_controller.rb
@@ -2,6 +2,10 @@ class Admin::EntriesController < ApplicationController
   before_filter :require_authentication
   
   def index
+    @entries = Entry.draft.paginate(
+      :page => (params[:page] || 1), :per_page => Entry.per_page)
+    
+    render :template => 'entries/index'
   end
 
   def new
