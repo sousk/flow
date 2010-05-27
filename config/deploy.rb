647feeb83849f3f2b8668e04c45bfc8c2d465c5c
sousk <sou.phphp@gmail.com>
2010-05-27 13:33:08 +0900

    fixed to work

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,12 +4,13 @@ set :repository,  "git://github.com/sousk/flow.git"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/home/www/sites/#{application}"
-set :user, 'rails'
+set :user, 'devel'
+set :use_sudo, false
+set :deploy_to, "/home/#{user}/sites/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
 
 role :app, "d.sousk.net"
 role :web, "d.sousk.net"
